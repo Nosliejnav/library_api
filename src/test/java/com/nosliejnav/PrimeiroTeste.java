22f38174010a9bbf34652057de5829fb8c03cc9e
Nosliejnav <nosliejnav@gmail.com>
2024-12-04 20:50:19 -0300

    Conhecendo a estrutura de um teste

--- src/test/java/com/nosliejnav/PrimeiroTeste.java
+++ src/test/java/com/nosliejnav/PrimeiroTeste.java
@@ -1,6 +1,21 @@
 package com.nosliejnav;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PrimeiroTeste {
 
+    @Test
+    public void estruturaDeUmTeste(){
+        // cenário
+        int numero1 = 10, numero2 = 5;
+
+        // execução
+        int resultado = numero1 + numero2;
+
+        //verificações
+        Assert.assertEquals(10, resultado);
+
+    }
 }
