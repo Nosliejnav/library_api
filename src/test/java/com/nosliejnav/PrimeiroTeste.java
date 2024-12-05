0b93f9a2d1feecce7b3de8f203255ea6baf10de3
Nosliejnav <nosliejnav@gmail.com>
2024-12-05 20:56:50 -0300

    Desafio da calculadora

--- src/test/java/com/nosliejnav/PrimeiroTeste.java
+++ src/test/java/com/nosliejnav/PrimeiroTeste.java
@@ -1,8 +1,6 @@
 package com.nosliejnav;
 
 import org.assertj.core.api.Assertions;
-
-
 import org.junit.Test;
 
 public class PrimeiroTeste {
@@ -10,14 +8,32 @@ public class PrimeiroTeste {
     @Test
     public void deveSomar2Numeros(){
         // cenário
+        Calculadora calculadora = new Calculadora();
         int numero1 = 10, numero2 = 5;
 
         // execução
-        int resultado = numero1 + numero2;
+        int resultado = calculadora.somar(numero1, numero2);
 
         //verificações
         Assertions.assertThat(resultado).isEqualTo(15);
+    }
 
+    @Test(expected = RuntimeException.class)
+    public void naoDeveSomarNumerosNegativos(){
+        //cenário
+        Calculadora calculadora = new Calculadora();
+        int num1 = -10, num2 =5;
+
+        //execução
+        calculadora.somar(num1, num2);
     }
 }
 
+class Calculadora {
+    int somar (int num, int num2){
+        if(num < 0 || num2 < 0){
+            throw new RuntimeException("Não é permitido somar numeros negativos.");
+        }
+        return num + num2;
+    }
+}
