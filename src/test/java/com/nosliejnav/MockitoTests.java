7ec1804434b8b778a82b1b0f596e4b459c729bdf
Nosliejnav <nosliejnav@gmail.com>
2024-12-06 20:53:44 -0300

    Outros recursos úteis do Mockito

--- src/test/java/com/nosliejnav/MockitoTests.java
+++ src/test/java/com/nosliejnav/MockitoTests.java
@@ -2,21 +2,32 @@ package com.nosliejnav;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith( MockitoJUnitRunner.class)
 public class MockitoTests {
+
+    @Mock
+    List<String> lista;
 
     @Test
     public void primeiroTesteMockito(){
-        List<String> lista = Mockito.mock(ArrayList.class);
-
         Mockito.when(lista.size()).thenReturn(2);
 
-        int size = lista.size();
+        int size = 0;
+        if(1 == 2){
+            size = lista.size();
+            size = lista.size();
+        }
 
-        Assertions.assertThat(size).isEqualTo(2);
+//        Assertions.assertThat(size).isEqualTo(2);
+        Mockito.verify(lista, Mockito.never()).size();
     }
 }
