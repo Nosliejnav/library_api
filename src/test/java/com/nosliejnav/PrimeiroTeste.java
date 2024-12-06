58658b5e489c735e283577f4d486e7ee01df7fa3
Nosliejnav <nosliejnav@gmail.com>
2024-12-05 22:33:03 -0300

    Testando uma calculadora

--- src/test/java/com/nosliejnav/PrimeiroTeste.java
+++ src/test/java/com/nosliejnav/PrimeiroTeste.java
@@ -1,15 +1,22 @@
 package com.nosliejnav;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrimeiroTeste {
 
+    Calculadora calculadora;
+    int numero1 = 10, numero2 = 5;
+
+    @Before
+    public void setUp(){
+        calculadora = new Calculadora();
+    }
+
     @Test
     public void deveSomar2Numeros(){
         // cenário
-        Calculadora calculadora = new Calculadora();
-        int numero1 = 10, numero2 = 5;
 
         // execução
         int resultado = calculadora.somar(numero1, numero2);
@@ -21,20 +28,17 @@ public class PrimeiroTeste {
     @Test(expected = RuntimeException.class)
     public void naoDeveSomarNumerosNegativos(){
         //cenário
-        Calculadora calculadora = new Calculadora();
         int num1 = -10, num2 =5;
 
         //execução
         calculadora.somar(num1, num2);
     }
 
-    // Inicio Desafio calculadora
+//     INICIO Desafio calculadora
 
     @Test
     public void deveSubtrair2Numeros(){
         // cenário
-        Calculadora calculadora = new Calculadora();
-        int numero1 = 10, numero2 = 5;
 
         // execução
         int resultado = calculadora.subtrair(numero1, numero2);
@@ -44,23 +48,8 @@ public class PrimeiroTeste {
     }
 
     @Test
-    public void deveDividir2Numeros(){
-        // cenário
-        Calculadora calculadora = new Calculadora();
-        int numero1 = 10, numero2 = 5;
-
-        // execução
-        int resultado = calculadora.dividir(numero1, numero2);
-
-        //verificações
-        Assertions.assertThat(resultado).isEqualTo(2);
-    }
-
-    @Test
     public void deveMultiplicar2Numeros(){
         // cenário
-        Calculadora calculadora = new Calculadora();
-        int numero1 = 10, numero2 = 5;
 
         // execução
         int resultado = calculadora.multiplicar(numero1, numero2);
@@ -69,8 +58,28 @@ public class PrimeiroTeste {
         Assertions.assertThat(resultado).isEqualTo(50);
     }
 
-    //Desafio calculadora Fim
-}
+    @Test
+    public void deveDividir2Numeros() {
+        // cenário
+
+        // execução
+        float resultado = calculadora.dividir(numero1, numero2);
+
+        //verificações
+        Assertions.assertThat(resultado).isEqualTo(2);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void naoDeveDividirPorZero(){
+        //cenario
+        int numero1 = 10, numero2 = 0;
+
+        //execução
+        calculadora.dividir(numero1, numero2);
+        }
+    }
+
+    //Desafio calculadora FIM
 
 class Calculadora {
     int somar (int num, int num2){
@@ -79,19 +88,19 @@ class Calculadora {
         }
         return num + num2;
     }
-
+//     INICIO Desafio calculadora
     int subtrair(int num, int num2){
         return num - num2;
-    }
-
-    int dividir(int num, int num2){
-        if(num < 0 || num2 < 0){
-            throw new RuntimeException("Não é permitido dividir numeros negativos.");
-        }
-        return num / num2;
     }
 
     int multiplicar(int num, int num2){
         return num * num2;
     }
+
+    float dividir(int num, int num2){
+        return num / num2;
+    }
+//    Desafio calculadora FIM
+
+
 }
