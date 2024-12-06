a46a9bce2f8d385140ba1b04eb30ba6d7a1bc265
Nosliejnav <nosliejnav@gmail.com>
2024-12-06 16:23:21 -0300

    Cadastro de Pessoas com TDD "deveAdiciconarUmaPessoa"

--- src/main/java/com/nosliejnav/CadastroPessoas.java
+++ src/main/java/com/nosliejnav/CadastroPessoas.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CadastroPessoas {
+
+    /*
+    deveCriarOCadastroDePessoas
+     */
     private List<Pessoa> pessoas;
 
     public CadastroPessoas() {
@@ -13,5 +17,10 @@ public class CadastroPessoas {
     public List<Pessoa> getPessoas() {
         return this.pessoas;
     }
-
+/*
+deveAdiciconarUmaPessoa
+ */
+    public void adicionar(Pessoa pessoa) {
+        this.pessoas.add(pessoa);
+    }
 }
--- src/test/java/com/nosliejnav/CadastroPessoasTest.java
+++ src/test/java/com/nosliejnav/CadastroPessoasTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 public class CadastroPessoasTest {
 
+    /*
+    deveCriarOCadastroDePessoas
+     */
     @Test
     public void deveCriarOCadastroDePessoas(){
         //cenário e execução
@@ -13,4 +16,25 @@ public class CadastroPessoasTest {
         //verificação
         Assertions.assertThat(cadastro.getPessoas()).isEmpty();
     }
+/*
+deveAdiciconarUmaPessoa
+ */
+    @Test
+    public void deveAdiciconarUmaPessoa(){
+        //Cenário
+        CadastroPessoas cadastroPessoas = new CadastroPessoas();
+        Pessoa pessoa = new Pessoa();
+
+        //execução
+        cadastroPessoas.adicionar(pessoa);
+
+        //verificação
+        Assertions.assertThat(cadastroPessoas.getPessoas())
+                .isNotEmpty()
+                .hasSize(1)
+                .contains(pessoa);
+    }
+    /*
+
+     */
 }
