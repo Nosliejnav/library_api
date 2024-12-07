1d01b9774c3b994f38f9b048e2e4e629687eb298
Nosliejnav <nosliejnav@gmail.com>
2024-12-07 20:06:59 -0300

    PrimeiroProjetoRest

---
+++ src/main/java/com/example/primeiroProjetoRest/PrimeiroProjetoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.primeiroProjetoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/primeiroProjetoRest/PrimeiroProjetoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiroProjetoRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
