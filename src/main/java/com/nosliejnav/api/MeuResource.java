98e60dc32fd3d06b9092d2a7097094bc3c9aaf90
Nosliejnav <nosliejnav@gmail.com>
2024-12-07 21:30:38 -0300

    basico para criação de API Rest na pratica

---
+++ src/main/java/com/nosliejnav/api/MeuResource.java
@@ -0,0 +1,17 @@
+package com.nosliejnav.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MeuResource {
+
+//    @RequestMapping(value = "/api/clientes/{id}", method = RequestMethod.GET)
+
+    @GetMapping("/api/clientes/{id}")
+    public Cliente obterDadosCliente(@PathVariable Long id){
+        System.out.println(String.format("Id recebido via url: %d", id));
+        Cliente cliente = new Cliente("Fulano", "000.000.000-00");
+        return cliente;
+
+    }
+}
