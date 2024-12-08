67889b830e513b176a2eac7d7b7821df8e5589e4
Nosliejnav <nosliejnav@gmail.com>
2024-12-07 21:57:55 -0300

    BAse para criação de API Rest na pratica 01

--- src/main/java/com/nosliejnav/api/MeuResource.java
+++ src/main/java/com/nosliejnav/api/MeuResource.java
@@ -1,12 +1,41 @@
 package com.nosliejnav.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MeuResource {
 
-//    @RequestMapping(value = "/api/clientes/{id}", method = RequestMethod.GET)
+//    @PostMapping("api/clientes")
+//    public ResponseEntity salvar(@RequestBody Cliente cliente){
+//        //service.save(cliente);
+//        return new ResponseEntity(cliente, HttpStatus.CREATED);
+//    }
 
+    @PostMapping("api/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody Cliente cliente){
+        //service.save(cliente);
+        return cliente;
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete( @PathVariable Long id){
+        //service.buscarPorId(id);
+        //service.delete(cliente);
+    }
+
+    @PutMapping("/api/clientes/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        //service.buscarPorId(id);
+        //service.update(cliente);
+        return cliente;
+    }
+
+
+//    @RequestMapping(value = "/api/clientes/{id}", method = RequestMethod.GET)
     @GetMapping("/api/clientes/{id}")
     public Cliente obterDadosCliente(@PathVariable Long id){
         System.out.println(String.format("Id recebido via url: %d", id));
