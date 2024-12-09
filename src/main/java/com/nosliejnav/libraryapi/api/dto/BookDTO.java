8c6696d49a15e0656c59312bdcad4e0faa5ad406
Nosliejnav <nosliejnav@gmail.com>
2024-12-09 20:36:59 -0300

    Utilizando os recursos Bean Validation e Exception Handler

--- src/main/java/com/nosliejnav/libraryapi/api/dto/BookDTO.java
+++ src/main/java/com/nosliejnav/libraryapi/api/dto/BookDTO.java
@@ -1,6 +1,11 @@
 package com.nosliejnav.libraryapi.api.dto;
 
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -8,8 +13,14 @@ import lombok.*;
 @AllArgsConstructor
 public class BookDTO {
     private Long id;
+
+    @NotEmpty
     private String title;
+
+    @NotEmpty
     private String author;
+
+    @NotEmpty
     private String isbn;
 
 
--- src/main/java/com/nosliejnav/libraryapi/api/resource/BookController.java
+++ src/main/java/com/nosliejnav/libraryapi/api/resource/BookController.java
@@ -1,10 +1,14 @@
 package com.nosliejnav.libraryapi.api.resource;
 
 import com.nosliejnav.libraryapi.api.dto.BookDTO;
+import com.nosliejnav.libraryapi.api.exception.ApiErros;
 import com.nosliejnav.libraryapi.model.entity.Book;
 import com.nosliejnav.libraryapi.service.BookService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,9 +25,17 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto){
+    public BookDTO create(@RequestBody @Valid BookDTO dto){
         Book entity = modelMapper.map( dto, Book.class);
         entity = bookService.save(entity);
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleValidationExceptions(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        return new ApiErros(bindingResult);
+
     }
 }
