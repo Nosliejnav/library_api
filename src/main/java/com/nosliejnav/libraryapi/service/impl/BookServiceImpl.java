62525392aff6f91d96ce52b7e516e42ac03d9bce
Nosliejnav <nosliejnav@gmail.com>
2024-12-11 19:32:02 -0300

    Desafio dos metodos delete e update

--- src/main/java/com/nosliejnav/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/nosliejnav/libraryapi/service/impl/BookServiceImpl.java
@@ -32,11 +32,18 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void delete(Book book) {
-
+        if (book == null || book.getId() == null){
+            throw new IllegalArgumentException("Book id cant be null.");
+        }
+        this.bookRepository.delete(book);
     }
 
     @Override
     public Book update(Book book) {
-        return null;
+
+        if (book == null || book.getId() == null) {
+            throw new IllegalArgumentException("Book id cant be null.");
+        }
+        return this.bookRepository.save(book);
     }
 }
